e50ff17eeb28cff8edcddb5feb1ba5b8f63ba805
Putera alk <150687092+Anuvbis12@users.noreply.github.com>
2025-10-09 17:20:04 +0700

    ブラックバリル

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3675914377"
+    serviceWorkerVersion: "3642113608"
   }
 });
